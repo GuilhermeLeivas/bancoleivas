d38fff13d2780cb2c573583b885d79f805f204a4
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-01-21 19:09:51 -0300

    Começando separação de PessoaJuridica e PessoaFisica;

--- src/main/java/br/com/leivas/bancoleivas/model/reg/Pessoa.java
+++ src/main/java/br/com/leivas/bancoleivas/model/reg/Pessoa.java
@@ -7,20 +7,17 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Getter
-@ToString
 @RequiredArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "REGPESSOA")
 @SequenceGenerator(name = "seqRegPessoa", sequenceName = "SEQREGPESSOA", allocationSize = 1)
-public class Pessoa extends BaseEntity {
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Pessoa extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "seqRegPessoa")
     private Long id;
-    @Column(length = 500)
-    private String nome;
-    @Column
-    private String documento;
-    @Temporal(TemporalType.DATE)
-    private Date dtNascimento;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "CADASTRONACIONALID", referencedColumnName = "ID", unique = true, nullable = true)
+    private CadastroNacional cadastroNacional;
 }
