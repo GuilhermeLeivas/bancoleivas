3394699bf2155b85d09d710fdc6d8556d3488e99
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-01-22 16:26:17 -0300

    Avançando na validação de documento. Só falta pegar os digitos verificadores.

--- src/main/java/br/com/leivas/bancoleivas/util/parser/CadastroNacionalParser.java
+++ src/main/java/br/com/leivas/bancoleivas/util/parser/CadastroNacionalParser.java
@@ -1,10 +1,26 @@
 package br.com.leivas.bancoleivas.util.parser;
 
+import br.com.leivas.bancoleivas.dto.reg.CadastroNacionalDTO;
+import br.com.leivas.bancoleivas.exception.custom.FalhaAoConverterCadastroNacional;
 import br.com.leivas.bancoleivas.model.reg.CadastroNacional;
+import br.com.leivas.bancoleivas.util.validator.CadastroNacionalValidator;
 
-public class CadastroNacionalParser implements Parser<String, CadastroNacional> {
+public class CadastroNacionalParser implements Parser<CadastroNacionalDTO, CadastroNacional> {
     @Override
-    public CadastroNacional parse(String input) {
+    public CadastroNacional parse(CadastroNacionalDTO cadastroNacionalDTO) {
+        try {
+            Boolean documentoEhValido = new CadastroNacionalValidator().isValid(cadastroNacionalDTO.getNumero());
+            if (documentoEhValido) {
+                CadastroNacional.TipoCadastroNacional tipoCadastroNacional = cadastroNacionalDTO.getTipo();
+                return CadastroNacional.builder()
+                        .numero(cadastroNacionalDTO.getNumero())
+                        .emissor(cadastroNacionalDTO.getEmissor())
+                        .tipo(tipoCadastroNacional)
+                        .build();
+            }
+        } catch (Exception ex) {
+            throw new FalhaAoConverterCadastroNacional();
+        }
         return null;
     }
 }
