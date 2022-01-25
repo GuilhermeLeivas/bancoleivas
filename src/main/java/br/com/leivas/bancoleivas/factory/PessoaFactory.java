15209609832fbce67b7b14b72baa918a2dcdd0e1
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-01-25 00:34:30 -0300

    Modificando teste em PessoaFactory;

--- src/main/java/br/com/leivas/bancoleivas/factory/PessoaFactory.java
+++ src/main/java/br/com/leivas/bancoleivas/factory/PessoaFactory.java
@@ -1,18 +1,15 @@
 package br.com.leivas.bancoleivas.factory;
 
 import br.com.leivas.bancoleivas.dto.reg.PessoaDTO;
-import br.com.leivas.bancoleivas.model.reg.CadastroNacional;
+import br.com.leivas.bancoleivas.dto.reg.PessoaFisicaDTO;
 import br.com.leivas.bancoleivas.model.reg.Pessoa;
 import br.com.leivas.bancoleivas.model.reg.PessoaFisica;
 import br.com.leivas.bancoleivas.model.reg.PessoaJuridica;
 
-import static br.com.leivas.bancoleivas.model.reg.CadastroNacional.TipoCadastroNacional.CPF;
-
 public class PessoaFactory implements IFactory<PessoaDTO, Pessoa> {
     @Override
     public Pessoa produce(PessoaDTO pessoaDTO) {
-        CadastroNacional.TipoCadastroNacional tipo = pessoaDTO.getCadastroNacional().getTipo();
-        return tipo == CPF ?
+        return pessoaDTO instanceof PessoaFisicaDTO ?
                 new PessoaFisica().fromDTO(pessoaDTO) : new PessoaJuridica().fromDTO(pessoaDTO);
     }
 }
