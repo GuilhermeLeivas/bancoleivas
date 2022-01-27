215a699c3a4d0952217882309e4c2880d3e84f57
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-01-26 21:20:49 -0300

    Começando Exception handler

--- src/main/java/br/com/leivas/bancoleivas/exception/handler/BancoLeivasExceptionHandler.java
+++ src/main/java/br/com/leivas/bancoleivas/exception/handler/BancoLeivasExceptionHandler.java
@@ -1,4 +1,34 @@
 package br.com.leivas.bancoleivas.exception.handler;
 
-public class BancoLeivasExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class BancoLeivasExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    @NonNull
+    protected ResponseEntity<Object> handleExceptionInternal(@NonNull Exception ex, Object body, @NonNull HttpHeaders headers,
+                                                             @NonNull HttpStatus status, @NonNull WebRequest request) {
+        Erro erro = new Erro("Falha interna no servidor, entre em contato com os Desenvolvedores!", ex.getCause().toString());
+        return super.handleExceptionInternal(ex, erro, headers, status, request);
+    }
+
+    // Facilitador para retornar mensagens de erro no corpo da resposta http.
+    private record Erro(String mensagemUsuario,
+                        String mensagemDesenvolvedor) {
+
+        public String getMensagemUsuario() {
+            return mensagemUsuario;
+        }
+
+        public String getMensagemDesenvolvedor() {
+            return mensagemDesenvolvedor;
+        }
+    }
 }
