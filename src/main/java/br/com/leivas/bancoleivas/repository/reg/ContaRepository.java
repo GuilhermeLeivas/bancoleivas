a3c090e5251050d5a3b8072799438b5261034d0e
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-01-30 13:53:22 -0300

    Alterando busca por conta.

--- src/main/java/br/com/leivas/bancoleivas/repository/reg/ContaRepository.java
+++ src/main/java/br/com/leivas/bancoleivas/repository/reg/ContaRepository.java
@@ -2,6 +2,7 @@ package br.com.leivas.bancoleivas.repository.reg;
 
 import br.com.leivas.bancoleivas.model.reg.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ import java.util.Optional;
 @Repository
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
+    @Query("SELECT conta FROM Conta conta WHERE conta.numero = ?1")
     Optional<Conta> findByNumero(Long numeroConta);
 }
