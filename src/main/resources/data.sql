d005f4bd4cf9dfb8921d10f402629a86a990ffe8
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-03 20:29:33 -0300

    Faltou no script inserir alguns modelos de transação

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,9 @@ values (nextval('seqauthpermissao'), 'Admin', 1, '2022-02-03', '2022-02-03');
 insert into authusuario(id, identificacao, username, senha, permissaoid, optmisticklock, dtcreate, dtupdate)
 values(nextval('seqauthusuario'), 'Administrador', 'Admin', '$2a$09$rWuGW7YUmGNuH/cel5po3uP1lV0PFpqLrHo7lyZaG9BbXfu21GvcK',
        currval('seqauthpermissao'), 1, '2022-02-03', '2022-02-03');
+
+insert into finmodelotransacao (id, codigo, nome, executacaodatransacao, transacaostrategy, optmisticklock, dtcreate, dtupdate)
+values (nextval('seqfinmodelotransacao'), 200, 'Depósito', 1, 'br.com.leivas.bancoleivas.strategy.DepositoStrategy', 1, '2022-01-30', '2022-01-30');
+
+insert into finmodelotransacao (id, codigo, nome, executacaodatransacao, transacaostrategy, optmisticklock, dtcreate, dtupdate)
+values (nextval('seqfinmodelotransacao'), 201, 'Transferência', 0, 'br.com.leivas.bancoleivas.strategy.TransferenciaStrategy', 1, '2022-01-30', '2022-01-30');
