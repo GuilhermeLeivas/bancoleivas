f248fd612a0ec70780bafee51327e83a77950b6e
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-04 01:27:31 -0300

    Retirando endpoint de atualização pessoa temporariamente.

--- src/main/java/br/com/leivas/bancoleivas/resource/PessoaResource.java
+++ src/main/java/br/com/leivas/bancoleivas/resource/PessoaResource.java
@@ -1,42 +1,42 @@
-package br.com.leivas.bancoleivas.resource;
-
-import br.com.leivas.bancoleivas.dto.reg.PessoaDTO;
-import br.com.leivas.bancoleivas.exception.handler.BancoLeivasExceptionHandler;
-import br.com.leivas.bancoleivas.model.reg.Pessoa;
-import br.com.leivas.bancoleivas.service.PessoaService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-
-@RestController
-@RequestMapping("/pessoa")
-public class PessoaResource {
-    private final PessoaService pessoaService;
-
-    public PessoaResource(PessoaService pessoaService) {
-        this.pessoaService = pessoaService;
-    }
-
-    @ApiOperation(value = "Endpoint utilizado para atualizar dados do CLIENTE.")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Dados do cliente foram atualizados", response = Pessoa.class),
-            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso", response = BancoLeivasExceptionHandler.Erro.class),
-            @ApiResponse(code = 404, message = "Cliente não possui cadastro no sistema", response = BancoLeivasExceptionHandler.Erro.class),
-            @ApiResponse(code = 500, message = "Foi gerada uma exceção", response = BancoLeivasExceptionHandler.Erro.class),
-    })
-    @PutMapping
-    @PreAuthorize("(hasAuthority('ADMIN') or hasAuthority('GERENTE') or hasAuthority('GERENTE')) and hasAuthority('SCOPE_read') and hasAuthority('SCOPE_write')")
-    public ResponseEntity<?> atualizaPessoa(@RequestBody @Valid PessoaDTO pessoaDTO) {
-        Pessoa pessoaAtualizada = this.pessoaService.atualizaPessoa(pessoaDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(pessoaAtualizada);
-    }
-}
+//package br.com.leivas.bancoleivas.resource;
+//
+//import br.com.leivas.bancoleivas.dto.reg.PessoaDTO;
+//import br.com.leivas.bancoleivas.exception.handler.BancoLeivasExceptionHandler;
+//import br.com.leivas.bancoleivas.model.reg.Pessoa;
+//import br.com.leivas.bancoleivas.service.PessoaService;
+//import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiResponse;
+//import io.swagger.annotations.ApiResponses;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.validation.Valid;
+//
+//@RestController
+//@RequestMapping("/pessoa")
+//public class PessoaResource {
+//    private final PessoaService pessoaService;
+//
+//    public PessoaResource(PessoaService pessoaService) {
+//        this.pessoaService = pessoaService;
+//    }
+//
+//    @ApiOperation(value = "Endpoint utilizado para atualizar dados do CLIENTE.")
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 200, message = "Dados do cliente foram atualizados", response = Pessoa.class),
+//            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso", response = BancoLeivasExceptionHandler.Erro.class),
+//            @ApiResponse(code = 404, message = "Cliente não possui cadastro no sistema", response = BancoLeivasExceptionHandler.Erro.class),
+//            @ApiResponse(code = 500, message = "Foi gerada uma exceção", response = BancoLeivasExceptionHandler.Erro.class),
+//    })
+//    @PutMapping
+//    @PreAuthorize("(hasAuthority('ADMIN') or hasAuthority('GERENTE') or hasAuthority('GERENTE')) and hasAuthority('SCOPE_read') and hasAuthority('SCOPE_write')")
+//    public ResponseEntity<?> atualizaPessoa(@RequestBody @Valid PessoaDTO pessoaDTO) {
+//        Pessoa pessoaAtualizada = this.pessoaService.atualizaPessoa(pessoaDTO);
+//        return ResponseEntity.status(HttpStatus.OK).body(pessoaAtualizada);
+//    }
+//}
